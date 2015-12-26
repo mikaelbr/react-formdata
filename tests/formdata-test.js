ff31fb1de2e186fa38acb4c9afc5dd49949a8512
Mikael Brevik <mikaelbre@gmail.com>
2015-12-26 13:11:42 +0100

    Removes jsdom as dependency

--- tests/formdata-test.js
+++ tests/formdata-test.js
@@ -1,19 +1,20 @@
 const { assert } = require('chai');
 const { renderIntoDocument } = require('react-addons-test-utils');
-const jsdom = require('jsdom');
 
 const formData = require('../src/formdata');
 
 describe('formdata', function () {
-  it('should be able to return values on mount', function (done) {
-    const MyForm = formData(() =>
-      <p><input id="a" type="text" readOnly="readOnly" value="Hello World"/></p>
-    );
+  describe('on mount', function () {
+    it('should be able to return values on mount', function (done) {
+      const MyForm = formData(() =>
+        <p><input id="a" type="text" readOnly="readOnly" value="Hello World"/></p>
+      );
 
-    const refListener = function (ref) {
-      assert.deepEqual(ref.getValues(), { a: 'Hello World' });
-      done();
-    };
-    renderIntoDocument(<MyForm ref={refListener} />);
+      const refListener = function (ref) {
+        assert.deepEqual(ref.getValues(), { a: 'Hello World' });
+        done();
+      };
+      renderIntoDocument(<MyForm ref={refListener} />);
+    });
   });
 });
